0fdb5dd2c83abcba714a0887227ea9c95a34749b
Chet Xu <510374040@qq.com>
2023-08-08 09:17:38 +0800

    update Message

---
+++ src/config.ts
@@ -0,0 +1,29 @@
+export default {
+  title: `ChetSerenade Blog`,
+  name: 'ChetSerenade',
+  since: '2021',
+  contacts: [
+    {
+      title: 'Twitter',
+      url: 'https://twitter.com/Lc0916x'
+    }
+  ],
+  navs: [
+    {
+      title: '首页',
+      url: '/'
+    },
+    {
+      title: '万象更新',
+      url: '/letters'
+    },
+    {
+      title: '素履以往',
+      url: '/consulting'
+    },
+    {
+      title: 'RSS',
+      url: '/rss.xml'
+    }
+  ]
+}
