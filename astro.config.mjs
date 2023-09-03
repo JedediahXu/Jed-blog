99c9dcdbe779f2e59ed4dc29de48d61a41bbad98
Chet Xu <510374040@qq.com>
2023-09-03 13:59:28 +0800

    fix Listening route

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 import vue from '@astrojs/vue';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chetserenade.info',
-  integrations: [tailwind(),vue()]
+  integrations: [tailwind(),vue(),svelte(),]
 });
