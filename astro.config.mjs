48748835fc0481ff6fcf09b81b268a8881742e26
Chet Xu <510374040@qq.com>
2023-09-03 16:39:11 +0800

    Delete the navigation bar Vue component

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
   site: 'https://chetserenade.info',
   integrations: [tailwind(),vue()]
 });
