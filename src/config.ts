c23b1c98b568052add6a8d50d7e3c99fc352e19e
Jed Xhz <510374040@qq.com>
2023-11-17 22:03:56 +0800

    add: 增加友链

--- src/config.ts
+++ src/config.ts
@@ -47,5 +47,9 @@ export default {
         label: 'Liuzepeng',
         link: 'https://www.liuzepeng.com',
       },
+      {
+        label: 'evex.one',
+        link: 'https://evex.one/',
+      },
   ]
 }
