779f0dc0cf9537c9bbd5d6f52287ff72037cc339
Jed Xhz <510374040@qq.com>
2023-12-08 11:20:19 +0800

    feat: : 文章字数统计不显示问题

--- src/data/calendar.ts
+++ src/data/calendar.ts
@@ -553,6 +553,12 @@ export  const data = {
           contributionCount: 1,
           color: "#9be9a8"
         },
+        {
+          weekday: 1,
+          date: "2023-12-08",
+          contributionCount: 1,
+          color: "#9be9a8"
+        },
       ]
     },
   ]
