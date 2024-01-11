28d2befdeccac5acaff1da9e73d356fecdf0eac8
Jed Xhz <510374040@qq.com>
2024-01-11 10:19:01 +0800

    fix:  optimize photoalbum

--- src/components/vue/Photoalbum.vue
+++ src/components/vue/Photoalbum.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { computed } from "vue";
 import probe from "probe-image-size";
-import { data } from "@/server/getters/photoalbum";
+import { data } from "../../server/getters/photoalbum.ts";
 
 async function updateImageSizes() {
   for (let i = 0; i < data.images.length; i++) {
